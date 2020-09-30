ef4ea10fd6a76f97438d743fd46dbc3f1c79e4bb
growydp <854862568@qq.com>
2020-09-30 21:05:47 +0800

    0930

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, cleanup } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Menu, { MenuProps } from './menu'
 import MenuItem from './menuItem'
@@ -34,6 +34,7 @@ let wrapper: RenderResult,
 describe('test Menu and MenuItem component', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps))
+    // 在 menu 中绑定了一个 data-testid
     menuElement = wrapper.getByTestId('test-menu')
     activeElement = wrapper.getByText('active')
     disabledElement = wrapper.getByText('disabled')
@@ -48,10 +49,24 @@ describe('test Menu and MenuItem component', () => {
   it('click items should change active and call the right callback', () => {
     const thirdItem = wrapper.getByText('normal')
     expect(thirdItem).toHaveClass('menu-item')
+    // active
+    expect(activeElement).toHaveClass('is-active')
+    // 模拟点击 thirdItem
     userEvent.click(thirdItem)
     expect(thirdItem).toHaveClass('menu-item is-active')
+    expect(activeElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+    // 模拟点击 disabledItem (有 disabled属性)
+    userEvent.click(disabledElement)
+    expect(disabledElement).not.toHaveClass('is-acitve')
+    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+    expect(thirdItem).toHaveClass('is-active')
   })
   it('should render vertical mode when mode is set to vertical', () => {
-
+    // 因为上面 beforeEach 已经存在 data-testid
+    cleanup()
+    const wrapper = render(generateMenu(testVertical))
+    const menuElement = wrapper.getByTestId('test-menu')
+    expect(menuElement).toHaveClass('menu-vertical')
   })
 })
